6ffc0aca3887fc7b9c88775cab3af43281318def
user <60804817+ichg4354@users.noreply.github.com>
2021-07-02 23:30:42 +0900

    finish graphQl wrapping REST with graphql

--- graphql/db.js
+++ graphql/db.js
@@ -2,14 +2,20 @@ import axios from "axios";
 
 const API_URL = "https://yts.mx/api/v2/list_movies.json";
 
-let MovieData;
-
-const getData = async () => {
+export const getData = async (limit, rating) => {
+  let REQUEST_URL = API_URL;
+  if (limit > 0) {
+    console.log(limit);
+    REQUEST_URL += `?limit=${limit}`;
+  }
+  if (rating > 0) {
+    REQUEST_URL += `&minimum_rating=${rating}`;
+    console.log(REQUEST_URL);
+  }
   const {
-    data: { data },
-  } = await axios.get(API_URL);
-  MovieData = data;
-  return MovieData;
+    data: {
+      data: { movies },
+    },
+  } = await axios.get(REQUEST_URL);
+  return movies;
 };
-
-getData();
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,13 +1,8 @@
-import { getUserData, getUserById, addUser, delUser } from "./db";
+import { getData } from "./db";
 
 const resolvers = {
   Query: {
-    crowd: () => getUserData(),
-    person: (_, args) => getUserById(args.id),
-  },
-  Mutation: {
-    addUser: (_, args) => addUser(args.name, args.age),
-    delUser: (_, args) => delUser(args.name),
+    movies: (_, { limit, rating }) => getData(limit, rating),
   },
 };
 
